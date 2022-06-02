cc790ce13bf7e2066ed39814168d6ded76aabe3e
Vinay Jaiswal <j04vinay@gmail.com>
2022-06-02 22:19:33 +0530

    commit

---
+++ Day 4/kLCM1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        if(n%2!=0){
+            cout<<1<<" "<<(n-1)/2<<" "<<(n-1)/2<<endl;
+        }else{
+            if(n%4==0){
+                cout<<n/2<<" "<<n/4<<" "<<n/4<<endl;
+            }else{
+                cout<<2<<" "<<(n-2)/2<<" "<<(n-2)/2<<endl;
+            }
+            
+        }
+    }
+    return 0;
+}
